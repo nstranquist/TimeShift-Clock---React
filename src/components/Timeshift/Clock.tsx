aa7afdfea0bd40e8f4252327223f076054682c89
Nico Stranquist <nstranquist@gmail.com>
2020-01-25 14:54:12 -0600

    add fullscreen toggling feature

--- src/components/Timeshift/Clock.tsx
+++ src/components/Timeshift/Clock.tsx
@@ -1,14 +1,36 @@
 import React, { useState, useEffect } from "react";
+// import {connect} from 'react-redux
 
-export const TS_Clock = ({
+// TODO: pass time in from props
+export const TS_Clock = ({}) => {
+  const [time, setTime] = useState<Date>(new Date());
 
-}) => {
-  const [time, setTime] = useState<Date>(new Date())
+  // Fullscreen
+  useEffect(() => {
+    const tsclockSection = document.getElementById("timeshift-clock");
+    const btnFullscreen = document.getElementById("btn-fullscreen");
+
+    const elem = document.documentElement; //document element
+    btnFullscreen!.addEventListener("click", function() {
+      if (elem.requestFullscreen) {
+        tsclockSection!.requestFullscreen();
+        tsclockSection!.classList.toggle("fullscreen-mode");
+      }
+      else {
+        document.exitFullscreen();
+      }
+    });
+  }, [])
+  
 
   return (
     <section id="timeshift-clock">
-      <div id="ts-clock-time" className="text-center">{time.getHours()}:{time.getMinutes()}:{time.getSeconds()}</div>
-      <div id="btn-fullscreen">Fullscreen: [X]</div>
+      <div id="ts-clock-time" className="text-center">
+        {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}
+      </div>
+      <div id="btn-fullscreen">
+        Fullscreen: [X]
+      </div>
       <div className="active-presets">Active: [none]</div>
     </section>
   );
