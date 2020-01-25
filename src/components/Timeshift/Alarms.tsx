e40ce3974c741ff15dc015bd2e1ea8f14792a486
Nico Stranquist <nstranquist@gmail.com>
2020-01-25 14:38:55 -0600

    re-implement section header toggles

--- src/components/Timeshift/Alarms.tsx
+++ src/components/Timeshift/Alarms.tsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const TS_Alarms = () => {
+  const [open, setOpen] = useState<boolean>(true)
+
+  const toggleHeaderOpen = () => {
+    setOpen(!open)
+  }
+
   return (
     <section className="alarms-container" id="alarms-section">
-      <header className="alarms-toggle section-header">
+      <header className="alarms-toggle section-header"
+        onClick={toggleHeaderOpen}>
         <h3 className="alarms-header">Alarms</h3>
       </header>
-      <div id="ts-alarms">
-        <div className="alarms-container">
+      {open && (
+        <div id="ts-alarms">
+          <div className="alarms-container">
+            <div className="alarm-item">
+              <div className="left-container">
+                <div className="alarm-name">Alarm 1</div>
+                <div className="alarm-days">M|T|W|R|F|S|S</div>
+              </div>
+              <div className="right-container">
+                <div className="alarm-status">
+                  <input
+                    type="checkbox"
+                    className="toggle-switch"
+                    id="checkbox-1"
+                  />
+                  {/* <label for="checkbox-1"></label> */}
+                </div>
+                {/* <div className="">Item</div>
+                            <div className="">Item</div> */}
+              </div>
+            </div>
+          </div>
           <div className="alarm-item">
             <div className="left-container">
-              <div className="alarm-name">Alarm 1</div>
+              <div className="alarm-name">Alarm 2</div>
               <div className="alarm-days">M|T|W|R|F|S|S</div>
             </div>
             <div className="right-container">
@@ -18,64 +45,46 @@ export const TS_Alarms = () => {
                 <input
                   type="checkbox"
                   className="toggle-switch"
-                  id="checkbox-1"
+                  id="checkbox-2"
                 />
-                {/* <label for="checkbox-1"></label> */}
+                {/* <label for="checkbox-2"></label> */}
               </div>
-              {/* <div className="">Item</div>
-                          <div className="">Item</div> */}
+            </div>
+          </div>
+          <div className="alarm-item">
+            <div className="left-container">
+              <div className="alarm-name">Alarm 3</div>
+              <div className="alarm-days">M|T|W|R|F|S|S</div>
+            </div>
+            <div className="right-container">
+              <div className="alarm-status">
+                <input
+                  type="checkbox"
+                  className="toggle-switch"
+                  id="checkbox-3"
+                />
+                {/* <label for="checkbox-3"></label> */}
+              </div>
+            </div>
+          </div>
+          <div className="alarm-item">
+            <div className="left-container">
+              <div className="alarm-name">Alarm 4</div>
+              <div className="alarm-days">M|T|W|R|F|S|S</div>
+            </div>
+            <div className="right-container">
+              <div className="alarm-status">
+                <input
+                  type="checkbox"
+                  className="toggle-switch"
+                  id="checkbox-4"
+                />
+                {/* <label for="checkbox-4"></label> */}
+              </div>
             </div>
           </div>
         </div>
-        <div className="alarm-item">
-          <div className="left-container">
-            <div className="alarm-name">Alarm 2</div>
-            <div className="alarm-days">M|T|W|R|F|S|S</div>
-          </div>
-          <div className="right-container">
-            <div className="alarm-status">
-              <input
-                type="checkbox"
-                className="toggle-switch"
-                id="checkbox-2"
-              />
-              {/* <label for="checkbox-2"></label> */}
-            </div>
-          </div>
-        </div>
-        <div className="alarm-item">
-          <div className="left-container">
-            <div className="alarm-name">Alarm 3</div>
-            <div className="alarm-days">M|T|W|R|F|S|S</div>
-          </div>
-          <div className="right-container">
-            <div className="alarm-status">
-              <input
-                type="checkbox"
-                className="toggle-switch"
-                id="checkbox-3"
-              />
-              {/* <label for="checkbox-3"></label> */}
-            </div>
-          </div>
-        </div>
-        <div className="alarm-item">
-          <div className="left-container">
-            <div className="alarm-name">Alarm 4</div>
-            <div className="alarm-days">M|T|W|R|F|S|S</div>
-          </div>
-          <div className="right-container">
-            <div className="alarm-status">
-              <input
-                type="checkbox"
-                className="toggle-switch"
-                id="checkbox-4"
-              />
-              {/* <label for="checkbox-4"></label> */}
-            </div>
-          </div>
-        </div>
-      </div>
+      )}
     </section>
   );
 };
--- src/components/Timeshift/Log.tsx
+++ src/components/Timeshift/Log.tsx
@@ -1,66 +1,75 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const TS_Log = () => {
+  const [open, setOpen] = useState<boolean>(true)
+
+  const toggleHeaderOpen = () => {
+    setOpen(!open)
+  }
+
   return (
-    <section className="log-container" id="log-section">
+    <section className="log-container" id="log-section"
+      onClick={toggleHeaderOpen}>
       <header className="log-toggle section-header">
         <h3 className="log-section-header">Log</h3>
       </header>
-      <div id="ts-log">
-        <div className="log-container">
-          <div className="log-entry">
-            <div className="log-header">
-              <h3 className="log-title">Title</h3>
-              <p className="log-date">Date</p>
+      {open && (
+        <div id="ts-log">
+          <div className="log-container">
+            <div className="log-entry">
+              <div className="log-header">
+                <h3 className="log-title">Title</h3>
+                <p className="log-date">Date</p>
+              </div>
+              <div className="log-body">
+                <p className="log-description">A shorter log description</p>
+              </div>
             </div>
-            <div className="log-body">
-              <p className="log-description">A shorter log description</p>
+            <div className="log-entry">
+              <div className="log-header">
+                <h3 className="log-title">Title</h3>
+                <p className="log-date">Date</p>
+              </div>
+              <div className="log-body">
+                <p className="log-description">
+                  This is the log body, often some sort of long description could
+                  go here or not, it is up to the individual logger that has the
+                  honor bestowed upon him or her to use this wonderfull app whose
+                  name is in fact TimeShift Clock, upon which using may or may not
+                  shift time in your favor
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="log-entry">
-            <div className="log-header">
-              <h3 className="log-title">Title</h3>
-              <p className="log-date">Date</p>
+            <div className="log-entry">
+              <div className="log-header">
+                <h3 className="log-title">Title</h3>
+                <p className="log-date">Date</p>
+              </div>
+              <div className="log-body">
+                <p className="log-description">A shorter log description</p>
+              </div>
             </div>
-            <div className="log-body">
-              <p className="log-description">
-                This is the log body, often some sort of long description could
-                go here or not, it is up to the individual logger that has the
-                honor bestowed upon him or her to use this wonderfull app whose
-                name is in fact TimeShift Clock, upon which using may or may not
-                shift time in your favor
-              </p>
+            <div className="log-entry">
+              <div className="log-header">
+                <h3 className="log-title">Title</h3>
+                <p className="log-date">Date</p>
+              </div>
+              <div className="log-body">
+                <p className="log-description">A shorter log description</p>
+              </div>
             </div>
-          </div>
-          <div className="log-entry">
-            <div className="log-header">
-              <h3 className="log-title">Title</h3>
-              <p className="log-date">Date</p>
-            </div>
-            <div className="log-body">
-              <p className="log-description">A shorter log description</p>
-            </div>
-          </div>
-          <div className="log-entry">
-            <div className="log-header">
-              <h3 className="log-title">Title</h3>
-              <p className="log-date">Date</p>
-            </div>
-            <div className="log-body">
-              <p className="log-description">A shorter log description</p>
-            </div>
-          </div>
-          <div className="log-entry">
-            <div className="log-header">
-              <h3 className="log-title">Title</h3>
-              <p className="log-date">Date</p>
-            </div>
-            <div className="log-body">
-              <p className="log-description">A shorter log description</p>
+            <div className="log-entry">
+              <div className="log-header">
+                <h3 className="log-title">Title</h3>
+                <p className="log-date">Date</p>
+              </div>
+              <div className="log-body">
+                <p className="log-description">A shorter log description</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };
