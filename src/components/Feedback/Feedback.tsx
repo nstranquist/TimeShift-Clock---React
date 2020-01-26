09734574756b01b9839605ea7dbd83ef715a769f
Nico Stranquist <nstranquist@gmail.com>
2020-01-26 17:14:56 -0600

    implement feedback feature (still needs working api)

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import '../../styles/components/Feedback.css'
 
 export const Feedback = () => {
+  const [feedbackData, setFeedbackData] = useState<any>({ name: '', email: '', message: '' })
+
+  const handleChange = (e: any) => {
+    setFeedbackData({
+      ...feedbackData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault()
+    // send the feedback data somewhere over an API
+    console.log('feedback data sent:', feedbackData)
+    clearFeedbackData()
+  }
+
+  const clearFeedbackData = () => setFeedbackData({ name: '', email: '', message: '' })
+
   return (
     <section className="page-feedback" id="feedback-section">
       <div className="feedback-container text-center">
         <h2>Feedback</h2>
-        <form className="feedback-form">
+        <form className="feedback-form" onSubmit={handleSubmit}>
           <p>Name</p>
-          <input type="text" name="Name" placeholder="Enter Name" />
+          <input type="text" name="name" placeholder="Enter Name" value={feedbackData.name} onChange={handleChange} />
           <p>Email</p>
-          <input type="text" name="Email" placeholder="Enter Email" />
+          <input type="email" name="email" placeholder="Enter Email" value={feedbackData.email} onChange={handleChange} />
           <p>Message</p>
-          <textarea name="message" placeholder="Type Here"></textarea>
+          <textarea name="message" placeholder="Type Here" value={feedbackData.message} onChange={handleChange}></textarea>
           <input
             className="submit-btn"
             type="submit"
-            name="submit"
+            // name="submit"
             value="Send"
           />
         </form>
